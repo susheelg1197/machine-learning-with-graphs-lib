47da1fe709626747664e9aa077ab0f8823c2ab57
Manas Bhole <62958063+ManasBhole@users.noreply.github.com>
2023-12-20 16:32:28 -0500

    Added graph isomorphism network in classification

---
+++ examples/use_gin.py
@@ -0,0 +1,15 @@
+import torch
+from ml_wg.classification.gin import GIN
+from torch_geometric.data import Data
+
+num_features = 3
+num_classes = 2
+model = GIN(num_features, num_classes)
+
+# Example data
+x = torch.randn((4, num_features))
+edge_index = torch.tensor([[0, 1], [1, 2], [2, 3], [3, 0]], dtype=torch.long).t()
+data = Data(x=x, edge_index=edge_index)
+
+output = model(data)
+print("Output from GIN: ", output)
